3b8b239c34b876e70e3b1bde286787b759655472
Brian Matherly <pez4brian@yahoo.com>
2012-08-11 23:36:10 -0500

    Updates to test scripts to support teamcity compatible output

--- test/test_libav_regression.sh
+++ test/test_libav_regression.sh
@@ -1,14 +1,11 @@
-#!/bin/bash -e
-wget https://raw.github.com/pez4brian/build-melt/master/build-melt.sh
-chmod 755 build-melt.sh
+#!/bin/bash
+#
+# This script compiles mlt against all branches of ffmpeg and libav.
+#
+# Author: Brian Matherly <pez4brian@yahoo.com>
+# License: GPL2
 
-init_config()
-{
-   if [ -f build-melt.conf ]; then rm build-melt.conf; fi
-   echo 'INSTALL_DIR="$PWD/melt"' >> build-melt.conf
-   echo 'SOURCE_DIR="$PWD/src"'   >> build-melt.conf
-   echo 'AUTO_APPEND_DATE=0'      >> build-melt.conf
-}
+source report_results.sh
 
 FFBRANCH[0]="ffmpeg_master"
 FFBRANCH[1]="ffmpeg_0.5"
@@ -17,11 +14,12 @@ FFBRANCH[3]="ffmpeg_0.7"
 FFBRANCH[4]="ffmpeg_0.8"
 FFBRANCH[5]="ffmpeg_0.9"
 FFBRANCH[6]="ffmpeg_0.10"
-FFBRANCH[7]="libav_master"
-FFBRANCH[8]="libav_0.5"
-FFBRANCH[9]="libav_0.6"
-FFBRANCH[10]="libav_0.7"
-FFBRANCH[11]="libav_0.8"
+FFBRANCH[7]="ffmpeg_0.11"
+FFBRANCH[8]="libav_master"
+FFBRANCH[9]="libav_0.5"
+FFBRANCH[10]="libav_0.6"
+FFBRANCH[11]="libav_0.7"
+FFBRANCH[12]="libav_0.8"
 
 CONFIG[0]='
    FFMPEG_HEAD=1
@@ -52,40 +50,108 @@ CONFIG[6]='
    FFMPEG_REVISION="origin/release/0.10"
    FFMPEG_PROJECT="ffmpeg"'
 CONFIG[7]='
+   FFMPEG_HEAD=0
+   FFMPEG_REVISION="origin/release/0.11"
+   FFMPEG_PROJECT="ffmpeg"'
+CONFIG[8]='
    FFMPEG_HEAD=1
    FFMPEG_PROJECT="libav"'
-CONFIG[8]='
+CONFIG[9]='
    FFMPEG_HEAD=0
    FFMPEG_REVISION="origin/release/0.5"
    FFMPEG_PROJECT="libav"
    FFMPEG_SUPPORT_LIBVPX=0'
-CONFIG[9]='
+CONFIG[10]='
    FFMPEG_HEAD=0
    FFMPEG_REVISION="origin/release/0.6"
    FFMPEG_PROJECT="libav"'
-CONFIG[10]='
+CONFIG[11]='
    FFMPEG_HEAD=0
    FFMPEG_REVISION="origin/release/0.7"
    FFMPEG_PROJECT="libav"'
-CONFIG[11]='
+CONFIG[12]='
    FFMPEG_HEAD=0
    FFMPEG_REVISION="origin/release/0.8"
    FFMPEG_PROJECT="libav"'
 
+print_help()
+{
+cat <<EOM
+   -------------------------------------------------------------------------
+      Usage: $(basename $0) [arg]
+         -e       Stop on [e]rror
+         -h       Print [h]elp and exit.
+         -t       Report in [t]eamcity format
+   -------------------------------------------------------------------------
+EOM
+}
 
-branch_count=${#CONFIG[@]}
-echo "$branch_count branches to build"
+parse_args()
+{
+   while getopts "eht" opt; do
+      case $opt in
+        e) set -e;;
+        h) print_help; exit 0;;
+        t) report_tc_format;;
+        *) echo "unknown arg: $opt"; print_help; exit 1;;
+      esac
+   done
+}
 
-for (( i=0; i<${branch_count}; i++ ));
-do
-   echo "echo building melt against ${FFBRANCH[$i]}"
-   init_config
-   for param in ${CONFIG[$i]} ; do
+init_suite()
+{
+   if [ -f build-melt.sh ]; then rm build-melt.sh; fi
+   wget https://raw.github.com/mltframework/mlt-scripts/master/build/build-melt.sh
+   chmod 755 build-melt.sh
+}
+
+cleanup_suite()
+{
+   rm build-melt.sh
+}
+
+init_test()
+{
+   if [ -f build-melt.conf ]; then rm build-melt.conf; fi
+   echo 'INSTALL_DIR="$PWD/melt"' >> build-melt.conf
+   echo 'SOURCE_DIR="$PWD/src"'   >> build-melt.conf
+   echo 'AUTO_APPEND_DATE=0'      >> build-melt.conf
+
+   for param in ${CONFIG[$1]} ; do
       echo $param >> build-melt.conf
    done
+}
 
-   ./build-melt.sh
-   tar -czvf ${FFBRANCH[$i]}.tar.gz melt
-   rm -Rf melt src
-done
+cleanup_test()
+{
+   rm -Rf melt src build-melt.conf
+}
+
+run_tests()
+{
+
+   branch_count=${#CONFIG[@]}
+   echo "$branch_count branches to build"
+
+   for (( i=0; i<${branch_count}; i++ ));
+   do
+      report_test_start ${FFBRANCH[$i]}
+      init_test $i
+
+      ./build-melt.sh
+      if [ ! -f "melt/melt" || $? != 0 ]; then
+         report_test_fail "Failed to build melt against ${FFBRANCH[$i]}"
+      fi
+      
+      cleanup_test
+      report_test_finish
+   done
+}
+
+parse_args "$@"
+report_suite_start "libav_regression"
+init_suite
+run_tests
+cleanup_suite
+report_suite_finish
 
--- test/test_yml.sh
+++ test/test_yml.sh
@@ -1,23 +1,65 @@
-echo "##teamcity[testSuiteStarted name='yml']"
+#!/bin/bash
+#
+# This script tests all the service metadata for correctness. It does this by 
+# using kwalify to test against each .yml file to be checked against the schema.
+# Additionally, it runs melt to make sure melt can parse the file.
+#
+# Author: Brian Matherly <pez4brian@yahoo.com>
+# License: GPL2
 
-for file in `find share/mlt -type f -name \*.yml`; do
-	file_name=${file##*/}
-	service_type=${file_name%%%%_*}
-	service_name=${file_name%*.yml}
-	service_name=${service_name#*_}
-	test_name=$service_type.$service_name
+source report_results.sh
 
-	echo "##teamcity[testStarted name='$test_name' captureStandardOutput='true']"
-	kwalify -f share/mlt/metaschema.yaml $file > /dev/null;
-	if [ $? -ne 0 ]; then
-		echo "##teamcity[testFailed name=$test_name' message='failed to run kwalify -f share/mlt/metaschema.yaml $file']"
-	fi
+print_help()
+{
+cat <<EOM
+   -------------------------------------------------------------------------
+      Usage: $(basename $0) [arg]
+         -e       Stop on [e]rror
+         -h       Print [h]elp and exit.
+         -t       Report in [t]eamcity format
+   -------------------------------------------------------------------------
+EOM
+}
 
-	./melt -query $service_type=$service_name > /dev/null;
-	if [ $? -ne 0 ]; then
-		echo "##teamcity[testFailed name='$test_name' message='failed to run melt -query $service_type=$service_name']"
-	fi
-	echo "##teamcity[testFinished name='$test_name']"
-done
+parse_args()
+{
+   while getopts "eht" opt; do
+      case $opt in
+        e) set -e;;
+        h) print_help; exit 0;;
+        t) report_tc_format;;
+        *) echo "unknown arg: $opt"; print_help; exit 1;;
+      esac
+   done
+}
 
-echo "##teamcity[testSuiteFinished name='yml']"
+
+run_tests()
+{
+   for file in `find share/mlt -type f -name \*.yml`; do
+	   file_name=${file##*/}
+	   service_type=${file_name%%%%_*}
+	   service_name=${file_name%*.yml}
+	   service_name=${service_name#*_}
+	   test_name=$service_type.$service_name
+
+      report_test_start "$test_name"
+
+	   kwalify -f share/mlt/metaschema.yaml $file > /dev/null;
+	   if [ $? -ne 0 ]; then
+         report_test_fail "failed to run kwalify -f share/mlt/metaschema.yaml $file"
+	   fi
+
+	   ./melt -query $service_type=$service_name > /dev/null;
+	   if [ $? -ne 0 ]; then
+		   report_test_fail "failed to run melt -query $service_type=$service_name"
+	   fi
+      report_test_finish
+   done
+}
+
+parse_args "$@"
+report_suite_start "yml"
+run_tests
+report_suite_finish
+
