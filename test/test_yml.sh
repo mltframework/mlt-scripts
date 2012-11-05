f0c7a8fcda660d72f1c1039164437f4fccabd966
Brian Matherly <pez4brian@yahoo.com>
2012-11-04 19:19:47 -0600

    Fix whitespace.

--- test/test_yml.sh
+++ test/test_yml.sh
@@ -37,23 +37,23 @@ parse_args()
 run_tests()
 {
    for file in `find share/mlt -type f -name \*.yml`; do
-	   file_name=${file##*/}
-	   service_type=${file_name%%_*}
-	   service_name=${file_name%*.yml}
-	   service_name=${service_name#*_}
-	   test_name=${service_type}_${service_name}
+      file_name=${file##*/}
+      service_type=${file_name%%_*}
+      service_name=${file_name%*.yml}
+      service_name=${service_name#*_}
+      test_name=${service_type}_${service_name}
 
       report_test_start "$test_name"
 
-	   kwalify -f share/mlt/metaschema.yaml $file > /dev/null;
-	   if [ $? -ne 0 ]; then
+      kwalify -f share/mlt/metaschema.yaml $file > /dev/null;
+      if [ $? -ne 0 ]; then
          report_test_fail "failed to run kwalify -f share/mlt/metaschema.yaml $file"
-	   fi
+      fi
 
-	   ./melt -query $service_type=$service_name > /dev/null;
-	   if [ $? -ne 0 ]; then
-		   report_test_fail "failed to run melt -query $service_type=$service_name"
-	   fi
+      ./melt -query $service_type=$service_name > /dev/null;
+      if [ $? -ne 0 ]; then
+         report_test_fail "failed to run melt -query $service_type=$service_name"
+      fi
       report_test_finish
    done
 }
