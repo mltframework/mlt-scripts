fb765af8093648489c2b8d07bdc2a5816208865f
Dan Dennedy <dan@dennedy.org>
2016-09-28 19:09:50 -0700

    Change teamcity/shotcut script to use docker.

--- teamcity/shotcut.sh
+++ teamcity/shotcut.sh
@@ -7,14 +7,41 @@
 set -o nounset
 set -o errexit
 
+function usage {
+  echo "Usage: $0 [-o target-os] [-s]"
+  echo "Where:"
+  echo -e "\t-o target-os\tDefaults to $(uname -s); use Win32 or Win64 to cross-compile"
+  echo -e "\t-s\t\tbuild SDK (Linux and Windows only)"
+}
+
+SDK=
+TARGET_OS=
+
+while getopts ":so:" OPT
+do
+  case $OPT in
+    s ) SDK=1;;
+    h ) usage
+        exit 0;;
+    o ) TARGET_OS=$OPTARG;;
+    * ) echo "Unknown option $OPT"
+        usage
+        exit 1;;
+  esac
+done
+
 # Get Script
-wget --no-check-certificate https://raw.githubusercontent.com/mltframework/shotcut/master/scripts/build-shotcut.sh
-chmod 755 build-shotcut.sh
-echo 'INSTALL_DIR="$(pwd)/shotcut"' >> build-shotcut.conf
-echo 'SOURCE_DIR="$(pwd)/src"' >> build-shotcut.conf
+if [ ! -f build-shotcut.sh ]
+then
+    wget --no-check-certificate https://raw.githubusercontent.com/mltframework/shotcut/master/scripts/build-shotcut.sh
+    chmod 755 build-shotcut.sh
+    echo 'INSTALL_DIR="$PWD/shotcut"' >> build-shotcut.conf
+    echo 'SOURCE_DIR="$PWD/src"' >> build-shotcut.conf
+    [ "$SDK" = "1" ] && echo 'DEBUG_BUILD=1' >> build-shotcut.conf
+fi
 
 # Run Script
-./build-shotcut.sh $@ 2>&1 | tee output.txt
+docker run -it --rm -v $PWD:/root/shotcut ddennedy/shotcut-build "$@" 2>&1 | tee output.txt
 
 # Check for need to retry
 if grep "Unable to git clone source for" output.txt
@@ -25,7 +52,7 @@ then
       sleep 60
       minutes=$((minutes-1))
    done
-   ./build-shotcut.sh $@ 2>&1 | tee output.txt
+   docker run -it --rm -v $PWD:/root/shotcut ddennedy/shotcut-build "$@" 2>&1 | tee output.txt
 fi
 
 if grep "Some kind of error occured" output.txt; then
@@ -33,8 +60,25 @@ if grep "Some kind of error occured" output.txt; then
    exit 1
 fi
 
+# For Windows sign and make installer
+if [ "$TARGET_OS" = "Win32" -o "$TARGET_OS" = "Win64" ] && [ "$SDK" != "1" ]
+then
+    echo "Making Windows installer"
+    cd shotcut/Shotcut
+    osslsigncode sign -pkcs12 "$HOME/CodeSignCertificates.p12" -readpass "$HOME/CodeSignCertificates.pass" \
+       -n "Shotcut" -i "http://www.meltytech.com" -t "http://timestamp.digicert.com" \
+       -in shotcut.exe -out shotcut-signed.exe
+    mv shotcut-signed.exe shotcut.exe
+    cd ..
+    makensis shotcut.nsi
+    osslsigncode sign -pkcs12 "$HOME/CodeSignCertificates.p12" -readpass "$HOME/CodeSignCertificates.pass" \
+        -n "Shotcut Installer" -i "http://www.meltytech.com" -t "http://timestamp.digicert.com" \
+        -in shotcut-setup.exe -out shotcut-setup-signed.exe
+    mv shotcut-setup-signed.exe shotcut-setup.exe
+    cd ..
+fi
+
 # Cleanup
-rm -Rf src
-rm *.sh
-rm *.conf
-rm output.txt
+rm -rf src *.sh *.conf output.txt
+[ "$TARGET_OS" = "Win32" -o "$TARGET_OS" = "Win64" ] && rm -rf shotcut shotcut.nsi
+
